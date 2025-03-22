45c418b484898363dd3207f90ef34fe0a1db6c14
Ansh <ansh.dhalla.ug20@nsut.ac.in>
2025-03-22 08:27:04 +0530

    excel bug fixed

--- knowledge/parse_excel.py
+++ knowledge/parse_excel.py
@@ -11,12 +11,12 @@ def extract_excel_to_markdown(file_path):
     return markdown_text
  
 
-file_path = '/Users/Apple/Desktop/Givery BP/Givery-Resume-Parsing/data/JP resume format 012.xlsx'  # Replace with your file path
+# file_path = '/Users/Apple/Desktop/Givery BP/Givery-Resume-Parsing/data/JP resume format 012.xlsx'  # Replace with your file path
 
-# Call the function and print the output
-markdown_output = extract_excel_to_markdown(file_path)
+# # Call the function and print the output
+# markdown_output = extract_excel_to_markdown(file_path)
 
-parsed_data_file = "/Users/Apple/Desktop/Givery BP/Givery-Resume-Parsing/output_parsed/parsed_data2.txt"
-with open(parsed_data_file, "w", encoding="utf-8") as f:
-    f.write(str(markdown_output))  
-    print(f"⛳️ Extraction saved to {parsed_data_file}")
+# parsed_data_file = "/Users/Apple/Desktop/Givery BP/Givery-Resume-Parsing/output_parsed/parsed_data2.txt"
+# with open(parsed_data_file, "w", encoding="utf-8") as f:
+#     f.write(str(markdown_output))  
+#     print(f"⛳️ Extraction saved to {parsed_data_file}")
